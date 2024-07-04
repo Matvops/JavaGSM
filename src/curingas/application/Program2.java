d4258d1f097771f98d50b36a8f6731407643e0a4
Matheus <matheusmartinscia@gmail.com>
2024-07-03 22:40:49 -0300

    Criei e implementei a classe para o problema 2 de curingas

---
+++ src/curingas/application/Program2.java
@@ -0,0 +1,28 @@
+package curingas.application;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
+		copy(myInts, myObjs);
+		System.out.println(myObjs.toString());
+		copy(myDoubles, myObjs);
+		System.out.println(myObjs.toString());
+	}
+	
+	public static void copy(List<? extends Object> list, List<Object> listO) {
+		if(!listO.isEmpty()) {
+			listO.removeAll(listO);
+		}
+		listO.addAll(list);
+	}
+
+}
