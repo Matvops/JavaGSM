3fe802f991a05e609aebbd73afd2a8dd4ffcedea
Matheus <matheusmartinscia@gmail.com>
2024-07-07 22:52:16 -0300

    Implementei a interface do site

--- src/exemplo/application/Program.java
+++ src/exemplo/application/Program.java
@@ -1,9 +1,12 @@
 package exemplo.application;
 
+import exemplo.entities.Site;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Site st = new Site();
+		st.interfaceLogin();
 
 	}
 
--- src/exemplo/entities/Site.java
+++ src/exemplo/entities/Site.java
@@ -3,16 +3,30 @@ package exemplo.entities;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
+import exemplo.services.Login;
+
 public class Site {
 	private Set<User> users = new TreeSet<>();
-
+	private Login lg = new Login();
+	
 	public void interfaceLogin() {
+		Scanner sc = new Scanner(System.in);
+		try {
+		System.out.print("Enter file full path: ");
+		String strPath = sc.nextLine();
+		users.addAll(lg.acessarArquivo(strPath));
+		System.out.println("Total users: " + users.size());
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} finally {
+			sc.close();
+		}
 		
 	}
-
 
 	public Set<User> getUsers() {
 		return users;
