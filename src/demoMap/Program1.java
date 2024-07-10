6adea2b0e6b296a54036e05e446e5fb76ae68258
Matheus <matheusmartinscia@gmail.com>
2024-07-09 23:40:20 -0300

    Demo implementada

--- src/demoMap/Program1.java
+++ src/demoMap/Program1.java
@@ -1,10 +1,32 @@
 package demoMap;
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		Map<String, String> cookies = new TreeMap<>();
+		String user, phone, email;
+		int i = 0;
+		while(i < 5) {
+			System.out.print("Digite o nome: ");
+			user = sc.nextLine();
+			System.out.print("Digite o phone: ");
+			phone = sc.nextLine();
+			System.out.print("Digite o email: ");
+			email = sc.nextLine();
+			cookies.put("user", user);
+			cookies.put("phone", phone);
+			cookies.put("email", email);
+			for(String key: cookies.keySet()) {
+				System.out.println(key + ": " + cookies.get(key));
+			}
+			i++;
+		}
+		sc.close();
 	}
 
 }
